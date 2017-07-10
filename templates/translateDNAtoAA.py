d59046b65fd4a33afe8adfc36208e27a09a92e2e
maxemil <max-emil.schon@icm.uu.se>
2017-07-10 08:00:20 +0200

    fix issue with empty fasta file in translate process

--- templates/translateDNAtoAA.py
+++ templates/translateDNAtoAA.py
@@ -5,13 +5,14 @@ import copy
 
 seqs = []
 seqs_codon_table_6 = []
-for seq in SeqIO.parse("${contig}", 'fasta'):
-    seq_prot = copy.deepcopy(seq)
-    seq_prot.seq = seq.seq.translate(table=1)
-    if '*' in seq_prot.seq:
-        seq_prot.seq = seq.seq.translate(table=6)
-        seqs_codon_table_6.append(seq_prot)
-        continue
-    seqs.append(seq_prot)
-    SeqIO.write(seqs, '%s.faa' % "${contig.baseName}", 'fasta')
-    #SeqIO.write(seqs_codon_table_6, '%s_table6.faa' % "${contig.baseName}", 'fasta')
+with open('%s.faa' % "${contig.baseName}" as out):
+    for seq in SeqIO.parse("${contig}", 'fasta'):
+        seq_prot = copy.deepcopy(seq)
+        seq_prot.seq = seq.seq.translate(table=1)
+        if '*' in seq_prot.seq:
+            seq_prot.seq = seq.seq.translate(table=6)
+            seqs_codon_table_6.append(seq_prot)
+            continue
+        seqs.append(seq_prot)
+        SeqIO.write(seqs, out, 'fasta')
+        #SeqIO.write(seqs_codon_table_6, '%s_table6.faa' % "${contig.baseName}", 'fasta')
