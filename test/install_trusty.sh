0585365b4ed6d899f55349df1e33590c8d4d737c
maxemil <max-emil.schon@icm.uu.se>
2018-01-22 13:36:38 +0100

    Singularity manual install

--- test/install_trusty.sh
+++ test/install_trusty.sh
@@ -1,8 +1,12 @@
-wget -O- http://neuro.debian.net/lists/trusty.de-md.libre | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
-sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
-
 sudo apt-get -qq update
-sudo apt-get install singularity-container
+
+VERSION=2.4
+wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
+tar xvf singularity-$VERSION.tar.gz
+cd singularity-$VERSION
+./configure --prefix=/usr/local
+make
+sudo make install
 
 wget -qO- get.nextflow.io | bash
 chmod +x nextflow
