24eb27f9b4be0379233c2625e1b75945e3a1e409
maxemil <max-emil.schon@icm.uu.se>
2018-01-22 13:01:24 +0100

    Add singularity-container repo for Ubuntu Trusty

--- test/install_trusty.sh
+++ test/install_trusty.sh
@@ -1,3 +1,6 @@
+wget -O- http://neuro.debian.net/lists/trusty.de-md.libre | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+
 sudo apt-get -qq update
 sudo apt-get install singularity-container
 
