5d1e275a766eb73fd555bb29d9f3ce2e08d1f4dc
maxemil <max-emil.schon@icm.uu.se>
2018-07-10 13:58:54 +0200

    Refactor visualization script

--- templates/visualize_decisions.py
+++ templates/visualize_decisions.py
@@ -6,33 +6,49 @@ import seaborn as sns
 from itertools import product
 import matplotlib.pyplot as plt
 
-threshold = 0.25
-infile = 'tree_decisions.txt'
 
-df = pd.read_csv(infile, sep='\\t', header=None, names=['Sample', 'Tree', 'Taxon', 'value'])
-df = df[df['value'] == True]
+def get_counts_sample_taxon(df):
+    taxa = set(df["Taxon"])
+    samples = set(df["Sample"])
+    counts = pd.DataFrame(columns=samples, index=taxa)
+    counts = counts.fillna(0)
 
-taxa = set(df["Taxon"])
-samples = set(df["Sample"])
-trees = set(df['Tree'])
+    for t,s in product(taxa, samples):
+        counts[s].loc[t] = df[(df["Sample"] == s) & (df["Taxon"] == t)].shape[0]
+    return counts
 
-df2 = pd.DataFrame(columns=samples, index=taxa)
-df2 = df2.fillna(0)
+def get_taxa_to_remove(counts, trees):
+    low = counts.apply(lambda x: all(x < threshold * len(trees)), axis=1)
+    low  = low[low == True].index
+    return low
 
-for t,s in product(taxa, samples):
-    df2[s].loc[t] = df[(df["Sample"] == s) & (df["Taxon"] == t)].shape[0]
+def make_barplot(df):
+    ax = sns.factorplot(hue="Taxon", x="Sample", data=df, kind="count", legend_out=True, size=8, aspect=1.1)
+    plt.savefig('decision_barplot.pdf')
+    plt.clf()
+    plt.cla()
 
-low = df2.apply(lambda x: all(x < threshold * len(trees)), axis=1)
-low  = low[low == True].index
+def make_heatmap(counts):
+    ax = sns.heatmap(counts, annot=True, cmap='Reds', xticklabels=True)
+    ax.figure.tight_layout()
+    plt.savefig('decision_heatmap.pdf')
 
-df = df[df.apply(lambda row: not any([row['Taxon'] == l for l in low]), axis=1)]
+def main(threshold, infile, filter_taxa=True):
+    df = pd.read_csv(infile, sep='\\t', header=None, names=['Sample', 'Tree', 'Taxon', 'value'], dtype=str)
+    df = df[df['value'] == 'True']
 
-ax = sns.factorplot(hue="Taxon", x="Sample", data=df, kind="count", legend_out=True, size=len(samples), aspect=1.1)
-plt.savefig('decision_barplot.pdf')
-plt.clf()
-plt.cla()
+    counts = get_counts_sample_taxon(df)
+    low = get_taxa_to_remove(counts, set(df['Tree']))
 
-df2 = df2.loc[[i not in low for i in df2.index]]
-ax = sns.heatmap(df2, annot=True, cmap='Reds', xticklabels=True, center=threshold * len(trees))
-ax.figure.tight_layout()
-plt.savefig('decision_heatmap.pdf')
+    df = df[df.apply(lambda row: not any([row['Taxon'] == l for l in low]), axis=1)]
+    make_barplot(df)
+
+    if filter_taxa:
+        counts = counts.loc[[i not in low for i in counts.index]]
+
+    make_heatmap(counts)
+
+if __name__ == '__main__':
+    threshold = 0.25
+    infile = "tree_decisions.txt"
+    main(threshold, infile, False)
