750d1bab3e41bda4e6ea3e5cf8bb5768798bfdc0
maxemil <max-emil.schon@icm.uu.se>
2019-08-06 13:52:49 +0200

    update travis to use singularity 3x

--- test/install_trusty.sh
+++ test/install_trusty.sh
@@ -1,12 +1,30 @@
 sudo apt-get -qq update
 
-VERSION=2.4
-wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
-tar xvf singularity-$VERSION.tar.gz
-cd singularity-$VERSION
-./configure --prefix=/usr/local
-make
-sudo make install
+
+export VERSION=1.12 OS=linux ARCH=amd64 && \
+  wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+  sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+  rm go$VERSION.$OS-$ARCH.tar.gz
+
+echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && \
+source ~/.bashrc
+
+export VERSION=3.3.0 && # adjust this as necessary \
+    wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
+    tar -xzf singularity-${VERSION}.tar.gz && \
+    cd singularity
+
+./mconfig && \
+    make -C builddir && \
+    sudo make -C builddir install
+#
+# VERSION=2.4
+# wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
+# tar xvf singularity-$VERSION.tar.gz
+# cd singularity-$VERSION
+# ./configure --prefix=/usr/local
+# make
+# sudo make install
 
 wget -qO- get.nextflow.io | bash
 chmod +x nextflow
